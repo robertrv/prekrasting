b3dea72606c91f24089922a5ec42c09049a930a9
Robert Ramírez Vique <robertrv@gmail.com>
2011-09-01 21:55:56 +0200

    new chapter started

---
+++ scjp/chp3/certification/Sample.java
@@ -0,0 +1,18 @@
+package certification;
+
+public class Sample {
+	
+	
+	
+	public static void main(String[] args) {
+		byte bb = (byte)2700;
+		System.out.println(bb);
+		float f = 234.56f;
+		short s = (short)f;
+		byte a  = 3;
+		byte b = 8;
+		byte c = (byte) (a + b);
+		s = (byte) 128;
+		System.out.println("s: " + s);
+	}
+}

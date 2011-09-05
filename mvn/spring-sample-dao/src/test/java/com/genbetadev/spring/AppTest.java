b94215e3ccd19223ef317b1988f7f92aeecf9ebc
Robert Ramírez Vique <robertrv@gmail.com>
2011-09-05 07:54:44 +0200

    some more changes for spring & certification

--- mvn/spring-sample-dao/src/test/java/com/genbetadev/spring/AppTest.java
+++ mvn/spring-sample-dao/src/test/java/com/genbetadev/spring/AppTest.java
@@ -1,29 +1,19 @@
 package com.genbetadev.spring;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
+import org.junit.Test;
+
+import junit.framework.Assert;
 import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+    public static TestSuite suite()
     {
         return new TestSuite( AppTest.class );
     }
@@ -31,8 +21,9 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        Assert.assertTrue( true );
     }
 }
--- scjp/chp3/certification/Sample.java
+++ scjp/chp3/certification/Sample.java
@@ -15,7 +15,27 @@ public class Sample {
 		s = (byte) 128;
 		System.out.println("s: " + s);
 		
-		int [][] array = new int [-1][-1];
+		int [][] array = new int [1][1];
 		System.out.println(array[0][0]);
+		String [] sa = new String[]{"", null, ""};
+		int[] aaa = {'a', 4, 03, 0x4, -3};
+		int[] ints = new int[]{1,2,3,};
+		char[] chars = {'2','c',0};
+		
+		Integer i3 = 1000;
+		Integer i4 = 1000;
+		if (i3 == i4) System.out.println("== ok");
+		if (i3.equals(i4)) System.out.println("are equals");
+		Sample ss = new Sample();
+		ss.doStuff(Long.valueOf("2"));
+		byte b2 = 5;
+		ss.doStuff(b2);
+	}
+	
+	public void doStuff(Long param) {
+		System.out.println("first: " + param);
+	}
+	public void doStuff(double param) {
+		System.out.println("second: " + param);
 	}
 }

51e760b2df8ca3925ec67228904461edbecca825
Robert Ramírez Vique <robertrv@gmail.com>
2012-05-10 08:19:17 +0200

    adding files

--- scjp/chp4/Sample.java
+++ scjp/chp4/Sample.java
@@ -1,10 +1,32 @@
 class Sample {
 	
-	public 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		int b = 3;
 		System.out.println("this is b:" + b);
 		System.out.println("this is b++:" + b++ + " another time: "+ (b = ++b));
-		System.out.println("args[0]: " + args[0]);
+		l("5.0 == 5L "+ (5.0 == 5L));
+		l("5.0 == 5 "+ (5.0 == 5));
+		l("5.1f == 5.1d "+ (5.1f == 5.1));
+		String[] bb = {};
+		l("test " + ('a' + 0));
+		int x = 1;
+		int y = 2;
+		if ((x++ > 1) && (y++ < 2)) {
+			l("inside");
+		}
+		l("value of y: "+y);
+		byte bin1 = Byte.parseByte("1010", 2);
+		byte bin2 = Byte.parseByte("0111", 2);
+		
+		l("bin1&bin2: "+ Byte.toString((byte)(bin1&bin2)));
+		l("bin1&bin2: "+ (bin1|bin2));
+		l("bin1&bin2: "+ (bin1^bin2));
+		l("condition: " + (97f == 'a'));
+		boolean bo = true;
+		l("cond: "+ (bo = true));
+	}
+	
+	public static void l(String arg) {
+		System.out.println(arg);
 	}
 }
--- scjp/chp5/Exercise5_3.java
+++ scjp/chp5/Exercise5_3.java
@@ -4,13 +4,29 @@ public class Exercise5_3 {
 		int test = 5 / 0;
 	}
 	
+	public static String reverse(String s) {
+		if (s.length() == 0) {
+			throw new IllegalStateException();
+		}
+		String reverseStr = "";
+		for (int i=s.length()-1; i>=0;--i){
+			reverseStr += s.charAt(i);
+		}
+		return reverseStr;
+	}
+	
 	public static void main(String[] args) {
 		try {
-			doException();
+			l("result: " + reverse(args[0]));
 		} catch (Exception exception) {
-			
+			l("Error at main !");
+			exception.printStackTrace();
 		} finally {
-			
+			l("main finished");
 		}
+	}
+	
+	public static void l(String arg){
+		System.out.println(arg);
 	}
 }

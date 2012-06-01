efeadf3efb32345870b36503857bb98e7b9a8c33
Robert Ramírez Vique <robertrv@gmail.com>
2012-06-01 18:32:00 +0200

    chapter 5, 6 & 7

--- scjp/chp5/Sample2.java
+++ scjp/chp5/Sample2.java
@@ -1,3 +1,4 @@
+
 public class Sample2 {
 	
 	public static void main(String[] args) {
@@ -9,17 +10,16 @@ public class Sample2 {
 		for(char c : args[0].toCharArray()) {
 			switch (c) {
 				case 'a':
-					System.out.println("is an a"); 
+					System.out.println("is an a");
 					break;
 				case 'b':
-					System.out.println("is a b"); 
+					System.out.println("is a b");
 					break;
 				default:
-					System.out.println("is another letter"); 
+					System.out.println("is another letter");
 					break;
-			}		
+			}
 		}
-		*/
 		int x = 5;
 		for (; x  > 4 || x < 3; x ++) {
 			if (x == 10000) {
@@ -30,5 +30,20 @@ public class Sample2 {
 			}
 		}
 		System.out.println("final value: " + x);
+		*/
+		foreach:
+		for (int j = 0; j < 5; j++) {
+			for (int k = 0; k < 3; k++) {
+				System.out.print("" + j);
+				if (j==3 && k==1) {
+					break foreach;
+				}
+				if (j==0 || j==2) {
+					break;
+				}
+			}
+		}
+		System.out.println("\nEnd!");
+
 	}
 }

d991c854b7eabe7c83a4fe020c7a1479698fa097
Robert Ramírez Vique <robertrv@gmail.com>
2012-07-15 13:06:47 +0200

    adding last kata: FizzBuzz

---
+++ kata/FizzBuzz/kata/home/FizzBuzzTest.py
@@ -0,0 +1,47 @@
+'''
+Created on 15/07/2012
+
+@author: rramirez
+'''
+import unittest
+from kata.home.FizzBuzz import FizzBuzz
+
+class Test(unittest.TestCase):
+
+    single_results = {
+                      1: 1,
+                      2: 2,
+                      3: "Fizz",
+                      4: 4,
+                      5: "Buzz",
+                      15: "FizzBuzz",
+                      44: 44,
+                      45: "Fizz",
+                      45: "FizzBuzz",
+                      55: "Buzz",
+                      99: "Fizz",
+                      }
+    def setUp(self):
+        self.cut = FizzBuzz()
+    
+    def testFizzBuzzPresent(self):
+        result = self.cut.generate(15) 
+        expected = result[len(result)-1]
+        self.assertEquals("FizzBuzz", expected)
+
+    def testMaxAsLetter(self):
+        self.assertRaises(ValueError, self.cut.generate, "maximum")
+    
+    def testMaxIsNotIgnored(self):
+        self.assertEqual(33, len(self.cut.generate(33)))
+    
+    def testSingleResults(self):
+        result = self.cut.generate()
+        for position, expected in self.single_results.items():
+            actual = result[position-1]
+            self.assertEqual(expected, actual, 
+                            "Error Postion: %s. Expected: %s. Actual: %s." % 
+                            (position, expected, actual))
+    
+if __name__ == "__main__":
+    unittest.main()

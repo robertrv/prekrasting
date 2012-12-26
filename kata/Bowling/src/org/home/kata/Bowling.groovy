ef3c82a5a6b32d5f3688884ff307bdaf4b8f8bf4
Robert Ramírez Vique <robertrv@gmail.com>
2012-12-26 20:18:51 +0100

    horrible solution after at least 4 pomodoros ... need lot of refactoring! I'll go through it later.

--- kata/Bowling/src/org/home/kata/Bowling.groovy
+++ kata/Bowling/src/org/home/kata/Bowling.groovy
@@ -15,6 +15,8 @@ class Bowling {
 				return 0
 			} else if (this == NUMBER) {
 				return Integer.parseInt(value.toString())
+			} else if (this == STRIKE) {
+				return 10
 			} else {
 				throw new IllegalStateException("Numberical value for a type which does not need it: " 
 					+ this)
@@ -25,7 +27,8 @@ class Bowling {
 	final String score
 	private int index
 	private int total
-	private TYPE previousFrameType
+	private TYPE lastFrameType
+	private boolean prePreStrike
 
 	Bowling(String score) {
 		if (score == null) {
@@ -34,8 +37,9 @@ class Bowling {
 		this.score = score
 		index = 0
 		total = 0
+		prePreStrike = false
 	}
-
+	
 	int calculate() {
 		while (index != score.length()) {
 			consumeFrame()
@@ -63,30 +67,92 @@ class Bowling {
 					// Should be numerical: miss or number, cannot be an STRIKE
 					secondShotValue = secondShotType.numericalValue(currentChar())
 				}
-				sumExtraPreviousValues(firstShotValue, secondShotValue)
-				this.previousFrameType = secondShotType
+				sumBonus(firstShotValue, secondShotValue)
+				this.lastFrameType = secondShotType
 				break
 	
 			case TYPE.STRIKE:
 				firstShotValue = 10
-				sumExtraPreviousValues(firstShotValue, secondShotValue)
-				this.previousFrameType = firstShotType
+				sumBonus(firstShotValue, secondShotValue)
+				this.lastFrameType = firstShotType
 			default:
 				break
 		}
 		index ++
 		
 		total += firstShotValue + secondShotValue
-	}
-	
-	void sumExtraPreviousValues(int firstTryValue, int secondTryValue) {
-		if (previousFrameType == TYPE.SPARE) {
-			total += firstTryValue
-		} else if (previousFrameType == TYPE.STRIKE) {
-			total += (firstTryValue + secondTryValue)
+		
+		if (isLastFrameBonus()) {
+			sumLastFrameBonus()
 		}
 	}
+	
+	/**
+	 * If current is not an strike or previous was not an STRIKE, we have
+	 * played two shots, so simply add bonus depending on the lastFrameType:
+	 * 		SPARE bonus: firstShot
+	 * 		STRIKE bonus: first + second shots
+	 *
+	 * If current previous was an strike, and previous previous was an strike:
+	 * 		Add extra bonus: first show
+	 *
+	 * To know that we are on this situation, save on prePreStrike that we did
+	 * not "bonused" all
+	 */
+	private void sumBonus(int firstTryValue, int secondTryValue) {
+		TYPE currentType = currentTryType()
 		
+		if (lastFrameType == TYPE.SPARE) {
+			total += firstTryValue
+		} else if (lastFrameType == TYPE.STRIKE) {
+			total += (firstTryValue + secondTryValue)
+		}
+		
+		if (prePreStrike) {
+			total += firstTryValue
+		}
+		
+		prePreStrike = lastFrameType == TYPE.STRIKE && currentType == TYPE.STRIKE
+		
+	}
+		
+	private boolean isLastFrameBonus() {
+		boolean isLast = false
+		
+		if (lastFrameType == TYPE.SPARE) {
+			isLast = this.score.length() == index + 1
+		} else if (lastFrameType == TYPE.STRIKE) {
+			isLast = this.score.length() == index + 2
+		}
+		return isLast;
+	}
+	
+	/**
+	 * We are on the last frame and can have previous values as SPARE or STRIKE.
+	 * If is SPARE should treat last shot as a "normal" spare
+	 * IF is a STRIKE, should treat first shot a a "normal" STRIKE, but last one just as numerical. 
+	 */
+	private void sumLastFrameBonus() {
+
+		// "Normal" treatment for the first shot
+		TYPE last = currentTryType()
+		// can be just: NUMBER, MISS or STRIKE (not spare!)
+		int first = last.numericalValue(currentChar())
+		sumBonus(first, 0)
+		
+		if (lastFrameType == TYPE.STRIKE) {
+			index ++ 
+			last = currentTryType()
+			if (last == TYPE.SPARE) {
+				total += (10 - first)
+			} else {
+				total += last.numericalValue(currentChar())
+			}
+		}
+		index ++
+
+	}
+
 	TYPE currentTryType() {
 		char current = currentChar()
 		if ('/' == current) {

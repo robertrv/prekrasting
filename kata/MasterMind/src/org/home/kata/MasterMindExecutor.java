de0b23f327e56e9f51f117c3c33c4a016cae6f50
Robert Ramírez Vique <robertrv@gmail.com>
2013-02-03 22:41:05 +0100

    a first test to get intellij wihting

--- kata/MasterMind/src/org/home/kata/MasterMindExecutor.java
+++ kata/MasterMind/src/org/home/kata/MasterMindExecutor.java
@@ -15,25 +15,24 @@ public class MasterMindExecutor {
         String solutionRemaining = "";
 
 
-
-        if ( currentTry.equals(solution)){
+        if (currentTry.equals(solution)) {
             result = "XXXX";
-        }else {
-           for (int i=0 ; i < currentTry.length(); i++){
-               if(solution.charAt(i) == currentTry.charAt(i)){
-                   result = result + "X";
-               } else {
-                   tryRemaining += currentTry.charAt(i);
-                   solutionRemaining += solution.charAt(i);
-               }
-           }
+        } else {
+            for (int i = 0; i < currentTry.length(); i++) {
+                if (solution.charAt(i) == currentTry.charAt(i)) {
+                    result = result + "X";
+                } else {
+                    tryRemaining += currentTry.charAt(i);
+                    solutionRemaining += solution.charAt(i);
+                }
+            }
 
             int index;
-            for (int i=0 ; i < tryRemaining.length(); i++){
+            for (int i = 0; i < tryRemaining.length(); i++) {
                 if (solutionRemaining.contains(String.valueOf(tryRemaining.charAt(i)))) {
                     result += "*";
                     index = solutionRemaining.indexOf(tryRemaining.charAt(i));
-                    solutionRemaining = solutionRemaining.substring(0, index) + solutionRemaining.substring(index+1);
+                    solutionRemaining = solutionRemaining.substring(0, index) + solutionRemaining.substring(index + 1);
                 }
             }
 
